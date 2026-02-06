fc954b596083bba3ff5853f659425b064a41f7ae
chenweicong <chenweicong@wiener.pro>
2026-02-06 14:32:00 +0800

    add sft.jsonl

--- scripts/run_grpo.sh
+++ scripts/run_grpo.sh
@@ -1,4 +1,7 @@
 uv run cs336_alignment/train_grpo.py \
-    # checkpoint.save_dir=./checkpoints/grpo_qwen2.5_math/grpo_off_policy_grpo200_b256_ep2 \
-    # train.run_name=grpo_off_policy_grpo200_b256_ep2 \
-    # train.n_grpo_steps=200 \
+    checkpoint.save_dir=./checkpoints/grpo_qwen2.5_math/grpo_off_policy_grpo200_b256_ep4 \
+    train.run_name=grpo_off_policy_grpo200_b256_ep4 \
+    train.n_grpo_steps=200 \
+    train.epoch_per_grpo_step=4 \
+    train.train_batch_size=256 \
+    train.gradient_accumulation_steps=128 \
